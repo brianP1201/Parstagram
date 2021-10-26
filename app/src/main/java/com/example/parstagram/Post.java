93c83ae38bdc50097bbdbe05a2183a7deca8793e
brianP1201 <brianpauldc@gmail.com>
2021-10-26 00:44:55 -0400

    Implemented bottom navigation fragments and swipe refresh layouts

--- app/src/main/java/com/example/parstagram/Post.java
+++ app/src/main/java/com/example/parstagram/Post.java
@@ -11,6 +11,7 @@ public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -18,6 +19,10 @@ public class Post extends ParseObject {
 
     public void setDescription(String description){
         put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
     }
 
     public void setImage(ParseFile parseFile){
